f4324a8b4f3adc1a688f3ca9e2dc685c4f68c174
Nikola Mitev <mitev.nkl@gmail.com>
2023-02-07 22:24:24 +0100

    can vote

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   def home
     @question = Question.new
+    @questions = Question.all.order(created_at: :desc)
+    @vote = Vote.new
   end
 
   def create_question
@@ -12,9 +14,23 @@ class PagesController < ApplicationController
     end
   end
 
+  def create_vote
+    @question = Question.find(params[:vote][:question_id])
+    @vote = Vote.new(vote_params)
+    if @vote.save
+      redirect_to root_path, notice: "Your vote was successfully recorded."
+    else
+      render :new
+    end
+  end
+
   private
 
   def question_params
     params.require(:question).permit(:title, :answers_text)
+  end
+
+  def vote_params
+    params.require(:vote).permit(:question_id, :answer, :voter_ip)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post '/questions', to: 'pages#create_question'
+  post 'create_vote', to: 'pages#create_vote', as: 'create_vote'
   # Defines the root path route ("/")
   # root "articles#index"
 end
